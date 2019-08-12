35342c026524903c23705a90c2925c0d240561a6
MatBrze <mateusz.brzezny@gmail.com>
2019-08-12 08:36:40 +0200

    Framework of program menu created

--- CM1.py
+++ CM1.py
@@ -5,9 +5,40 @@ from tkinter import messagebox as msb
 from PIL import ImageTk, Image
 from functools import partial
 
+
 root = tk.Tk()
 root.title("Copy Master 5000")
 root.configure(background='#64727f')
+menubar = tk.Menu(root)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="New")
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_command(label="Save as...")
+filemenu.add_command(label="Close")
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = tk.Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo")
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+editmenu.add_command(label="Delete")
+editmenu.add_command(label="Select All")
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+helpmenu = tk.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index")
+helpmenu.add_command(label="About...")
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
 
 
 def resource_path(relative_path):
